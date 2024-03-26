1f8d0a79c6620e1fa0bf5b1c1e1f2188c4364371
WvDeldenHR <wvdelden99@outlook.com>
2024-03-26 14:34:52 +0100

    icon fix

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -3,8 +3,8 @@ import { Image, Text, View } from "react-native";
 export function Header() {
     return (
         <View className="items-center pb-4 bg-primary-dark">
-            <Image className="w-18 h-12" 
-                    source={require('./../../assets/icons/icon_logo.png')} />
+            <Image className="w-16 h-12" 
+                    source={require('./../../assets/icons/icon_logo_01.png')} />
         </View>
     );
 }
