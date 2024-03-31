9db729dceeb2b57ec80348fdddcd7299b3551e77
WvDeldenHR <wvdelden99@outlook.com>
2024-03-31 20:23:16 +0200

    front-end setup

---
+++ components/layout/PostCardCarousel.jsx
@@ -0,0 +1,22 @@
+import { Image, Text, View } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+import { colors } from './../../assets/styles/Styles';
+
+export function PostCardCarousel({uri}) {
+    return (
+        <View className="relative mr-5">
+            <Image className="rounded-lg w-[150px] h-[200px]"
+                    source={{uri}}/>
+
+            {/* <LinearGradient
+                    colors={['rgba(0,0,0,0)', colors.primaryLightColor]}
+                /> */}
+            
+            <LinearGradient className="absolute rounded-b-lg w-full h-full"
+                            colors={['rgba(0,0,0,0)', colors.primaryLightColor]} />
+            <View className="absolute p-2 bottom-0 left-0 right-0">
+                <Text className="text-xs text-center font-semibold text-white">Dit is de titel van een nieuwsartikel voor NOS</Text>
+            </View>
+        </View>
+    );
+}
