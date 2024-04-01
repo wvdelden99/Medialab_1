70cc4ecc9b882159b7721486c67eaafb09abd0c9
senalisa <senababacan@hotmail.com>
2024-04-01 22:50:40 +0200

    comittttt

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import NavigationStack from './navigation/AppNavigation'
-
-export default function App() {
-
-    return (
-            <NavigationStack />
-    );
-}
--- assets/styles/Styles.jsx
+++
@@ -1,7 +0,0 @@
-// Colors
-export const colors = {
-    primaryLightColor: '#334767',
-    primaryColor: '#0C213D',
-    primaryDarkColor: '#051326',
-    secondaryColor: '#FD7500',
-};
