bc9a0d7b466ba1b340b966b4cfc761555a58f913
senalisa <senababacan@hotmail.com>
2024-04-01 13:17:06 +0200

    User preference

--- views/Discover.jsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export function Discover() {
-    return (
-        <View>
-            <Text className="text-white">Discover</Text>
-        </View>
-    );
-}
